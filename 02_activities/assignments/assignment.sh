aecd8362e8fed9c0b9e1e86916f83970f60e06b4
Anodymous1 <andy.co.yu@gmail.com>
2026-05-05 14:29:48 -0400

    use ./, tried fixing, works

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -33,13 +33,14 @@ mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
 
-mv raw./rawdata ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
-ls ./data/raw/
+ls ./data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
+mkdir ./data/processed
 mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy alserver log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
@@ -50,7 +51,6 @@ cp ./data/raw/*user*.log ./data/processed/user_logs
 cp ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-cd ..
 rm ./data/raw/*ipaddr*.*
 rm ./data/processed/user_logs/*ipaddr*.*
 
